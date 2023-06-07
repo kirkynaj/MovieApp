163d83a788ec6be46508dbef7849b11da4299c63
Kirk Garcia <janngarcia24@gmail.com>
2023-06-07 09:57:08 +0800

    ressolved UI issue

--- src/pages/Devs.jsx
+++ src/pages/Devs.jsx
@@ -1,58 +1,51 @@
 import React from "react";
 
-import { Box, Typography, Container, Grid, Avatar, Card } from "@mui/material";
+import { Box, Typography, Container, Avatar, Card } from "@mui/material";
 import kirkPic from "../assets/16712217.jpeg";
 import OrwillePic from "../assets/Orwille.png";
 import Footer from "../components/Footer";
 
-
 const Devs = () => {
   return (
     <>
-      <Box >
-        <Typography 
-        variant="h3" 
-        align="center"
-        padding={10}
-        >
+      <Box>
+        <Typography variant="h3" align="center" padding={10}>
           Two devs one project
         </Typography>
-        <Container style={{ display: 'flex', justifyContent: 'center' }}
-        
-        maxWidth="sm">
+        <Container
+          style={{ display: "flex", justifyContent: "center" }}
+          maxWidth="sm"
+        >
           <img src="" alt="" />
           <Box container spacing={20} align="center" direction="column">
-           
-              
-                <Card>
-                  <Avatar
-                    alt="Dev Name"
-                    src={kirkPic}
-                    sx={{ width: 400, height: 400 }}
-                  />
-                  <Typography variant="h3" align="center"
-                  padding={5}>Kirk</Typography>
-                  <Typography variant="p"
-                  padding={5}>
-                  Never stop learning... Opportunities may come.
-                  </Typography>
-                </Card>
-              
-            
-            
-              <Card justifyContent="center" alignItems="center" padding={10}>
-                <Avatar
-                  
-                  alt="Dev Name"
-                  src={OrwillePic}
-                  sx={{ width: 400, height: 400 }}
-                />
-                <Typography variant="h3" align="center" padding={5}>Orwille</Typography>
-                <Typography variant="p" align="center" padding={5}>
-                Graphic designer, web developer in the making, outdoor enthusiast
-                </Typography>
-              </Card>
-           
+            <Card>
+              <Avatar
+                alt="Dev Name"
+                src={kirkPic}
+                sx={{ width: 400, height: 400 }}
+              />
+              <Typography variant="h3" align="center" padding={5}>
+                Kirk
+              </Typography>
+              <Typography variant="p" padding={5}>
+                Never stop learning... Opportunities may come.
+              </Typography>
+            </Card>
+
+            <Card justifyContent="center" alignItems="center" padding={10}>
+              <Avatar
+                alt="Dev Name"
+                src={OrwillePic}
+                sx={{ width: 400, height: 400 }}
+              />
+              <Typography variant="h3" align="center" padding={5}>
+                Orwille
+              </Typography>
+              <Typography variant="p" align="center" padding={5}>
+                Graphic designer, web developer in the making, outdoor
+                enthusiast
+              </Typography>
+            </Card>
           </Box>
         </Container>
       </Box>
